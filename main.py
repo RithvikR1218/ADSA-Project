8efdaa6c874606c143c52c4443450a28f6f85745
RithvikR1218 <rithvik.ravilla@gmail.com>
2023-04-21 15:06:56 +0530

    add:menu file

---
+++ main.py
@@ -0,0 +1,7 @@
+# make the menu here
+
+while True:
+    print("Select option")
+    print("1 for inserting string")
+    print("2 -> Exit")
+    #Play around with the menu and use the functions in the other file
